65678ba2d5d1ae6b9d86bc9d3e4dfa1f4c48ee07
Carleno <caquinti@hotmail.com>
2011-06-30 00:53:51 -0300

    teste teste

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Estoque::Application.config.secret_token = 'f3fad81a2522d31edda21a48f36005ad9c9d8cbcd88532b6a4ed1e30c0c610c2ec55887471228090cd62d136c3a23867349e82051210bbdd078dd55f6cb8b528'
